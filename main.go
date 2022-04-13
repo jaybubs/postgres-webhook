1ef8d684ca02bec0c3a03519cfd1ccb4d0744cba
jaybubs <jkubofficial@gmail.com>
2022-04-13 19:11:38 +0800

    moved functions out of main

--- main.go
+++ main.go
@@ -16,11 +16,22 @@ import (
 	"github.com/lib/pq"
 )
 
+func Create_Listener(connection string, tvars *Template_vars) (*pq.Listener, error) {
+	prob := func(ev pq.ListenerEventType, err error) {
+		CE(err)
+	}
+	listener := pq.NewListener(connection, 1*time.Second, 60*time.Second, prob)
+	err := listener.Listen(tvars.Pgchannel)
+	CE(err)
+	fmt.Println("listener created")
+	return listener, nil
+}
+
 /*
 	The listener consists of two cases: a notification is sent, or time has elapsed
 	this will ensure that the connection is being kept alive. If desired "Pl(pinging)" and "load" can be uncommented and returned in the second case for debugging purposes
 */
-func listen(l *pq.Listener,ch chan<- string) {
+func Listen(l *pq.Listener,ch chan<- string) {
 	for {
 		select {
 		// listener comes with a channel
@@ -42,7 +53,7 @@ func listen(l *pq.Listener,ch chan<- string) {
 /*
 	Here we push the channel contents to the webhook everytime new content is received, set up as to run forever in a similar manner to the listener, but independent of it (rather than embedding the webhook directly inside the listener)
 */
-func push(ch <-chan string) {
+func Push(ch <-chan string) {
 	for {
 		// let's not spam the webhook endpoint and only post when there's actual content
 		select {
@@ -74,16 +85,32 @@ type Template_vars struct {
 	Json_column string
 }
 
+func Parse_and_exec(filename string, tvars *Template_vars, db *sql.DB) error {
+
+	tmpl, err := template.ParseFiles(filename)
+	CE(err)
+	// save to bytes
+	var buffalo bytes.Buffer
+	tmpl.Execute(&buffalo, tvars)
+	// and back to string for db.exec
+	_, err = db.Exec(buffalo.String())
+	CE(err)
+
+	return nil
+
+}
+
 func main() {
 
 	// make config struct accessible
 	errr := config.Read_config()
-	ce(errr)
+	CE(errr)
 
 	// connection string is created based off of the config.json, the password should normally not be stored in plaintext, but the security implementation is out of scope of this work
 	connection := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", config.Pguser, config.Pgpass, config.Pgdb)
+	// db is a pointer already!
 	db, err := sql.Open("postgres", connection)
-	ce(err)
+	CE(err)
 
 	// one struct for keycloak login events and one for admin events
 
@@ -98,68 +125,29 @@ func main() {
 		Json_column: "representation",
 	}
 
-	// string load is cast to bytes buffer because of io.reader method, only need to declare once and overwrite
-	var buf bytes.Buffer
-	var create_function string
-	var create_trigger string
-	var tmpl *template.Template
+	err = Parse_and_exec("./create_function.sql.tmpl", &login_event, db)
+	CE(err)
+	err = Parse_and_exec("./create_trigger.sql.tmpl", &login_event, db)
+	CE(err)
+	err = Parse_and_exec("./create_function.sql.tmpl", &admin_event, db)
+	CE(err)
+	err = Parse_and_exec("./create_trigger.sql.tmpl", &admin_event, db)
+	CE(err)
 
-	// set up function for login events
+	event_listener, err := Create_Listener(connection, &login_event)
+	CE(err)
+	admin_event_listener, err := Create_Listener(connection, &admin_event)
+	CE(err)
 
-	tmpl, err = template.ParseFiles("./create_function.sql.tmpl")
-	tmpl.Execute(&buf, login_event)
-	// and back to string for db.exec purpose
-	create_function = buf.String()
-	_, err = db.Exec(create_function)
-	ce(err)
-
-	// set up trigger for login events
-
-	tmpl, err = template.ParseFiles("./create_trigger.sql.tmpl")
-	tmpl.Execute(&buf, login_event)
-	create_trigger = buf.String()
-	_, err = db.Exec(create_trigger)
-	ce(err)
-
-	// set up function for admin events
-
-	tmpl, err = template.ParseFiles("./create_function.sql.tmpl")
-	// have to send string to bytes buffer because of io.reader method
-	tmpl.Execute(&buf, admin_event)
-	// and back to string for db.exec purpose
-	create_function = buf.String()
-	_, err = db.Exec(create_function)
-	ce(err)
-
-	// set up trigger for admin events
-
-	tmpl, err = template.ParseFiles("./create_trigger.sql.tmpl")
-	tmpl.Execute(&buf, admin_event)
-	create_trigger = buf.String()
-	_, err = db.Exec(create_trigger)
-	ce(err)
-
-	// callback fx for fatal error
-	problem := func(ev pq.ListenerEventType, err error) {
-		ce(err)
-	}
-
-	event_listener := pq.NewListener(connection, 1*time.Second, 60*time.Second, problem)
-	err = event_listener.Listen(login_event.Pgchannel)
-	admin_event_listener := pq.NewListener(connection, 1*time.Second, 60*time.Second, problem)
-	err = admin_event_listener.Listen(admin_event.Pgchannel)
-	ce(err)
-	fmt.Sprintf("Listening on: %s",config.Webhook_url)
-	
 	event_channel := make(chan string)
 	admin_channel := make(chan string)
 
 	//create event and admin event listeners asynchronously
-	go listen(event_listener, event_channel)
-	go push(event_channel)
+	go Listen(event_listener, event_channel)
+	go Push(event_channel)
 
-	go listen(admin_event_listener, admin_channel)
-	go push(admin_channel)
+	go Listen(admin_event_listener, admin_channel)
+	go Push(admin_channel)
 
 	// for loop to keep the program running
 	for {
@@ -170,7 +158,7 @@ func main() {
 }
 
 // error wrapper function
-func ce(err error) {
+func CE(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
